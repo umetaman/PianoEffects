6a80629a19a8783f56c66c44bc4eea3db58589f8
umetaman <info@umetaman.com>
2020-02-29 16:57:18 +0900

    MIDIと連携してVisual Effect Graphに持って行けた

---
+++ Assets/Scripts/NoteController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+using MidiJack;
+
+public class NoteController : MonoBehaviour
+{
+    public VisualEffect vfxRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MidiMaster.noteOnDelegate = OnNoteReceived;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnNoteReceived(MidiChannel channel, int note, float velocity)
+    {
+        vfxRef.SetVector3("GeneratePosition", new Vector3(note * 0.1f, 0, 0));
+        vfxRef.SendEvent("Note");
+    }
+}
