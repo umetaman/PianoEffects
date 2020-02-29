0ba0dd22a86ad8befc44dcfcb87a54e31637fc15
umetaman <info@umetaman.com>
2020-02-29 13:54:16 +0900

    Gridに並べてどうこうするやつを試した

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+[ExecuteInEditMode]
+public class GridController : MonoBehaviour
+{
+    public VisualEffect vfxRef;
+
+    public Vector2 ScreenSize;
+    public Vector2 ScreenPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScreenSize = new Vector2(Screen.width, Screen.height);
+        ScreenPosition = Input.mousePosition;
+
+        vfxRef.SetVector2("ScreenSize", ScreenSize);
+        vfxRef.SetVector2("ScreenPosition", ScreenPosition);
+    }
+}
